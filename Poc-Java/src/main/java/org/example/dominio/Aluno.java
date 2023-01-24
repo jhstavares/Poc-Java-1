eed08ab1d3f1e7a9e4a2e7ab269d3c290436bb3c
jhtavares_01@htmail.com <jhstavares_01@hotmail.com>
2023-01-24 14:15:24 -0300

    Feat: ClasseAluno

---
+++ Poc-Java/src/main/java/org/example/dominio/Aluno.java
@@ -0,0 +1,13 @@
+package org.example.dominio;
+
+public class Aluno {
+
+    private String nome;
+    private String materia;
+    private int anoEscolar;
+    private int idade;
+    private int matricula;
+    private Double nota;
+
+
+}
